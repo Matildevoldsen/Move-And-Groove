89cbe307c57936714dcea84b0f5e7347ef8fed68
Matilde Wittrup Enevoldsen <matilde.enevoldsen19@gmail.com>
2019-03-28 16:20:33 +0000

    - Add Bootstrap to devise layouts. - Setup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
+  get 'home/index'
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
